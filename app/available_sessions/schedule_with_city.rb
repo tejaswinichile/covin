57834c9c8976297c3fd8d5a53535b1cfdc147bb2
Tejaswini <tejaswini.chile@e2open.com>
2021-05-06 12:26:07 +0530

    Updated the code for 403 error

--- app/available_sessions/schedule_with_city.rb
+++ app/available_sessions/schedule_with_city.rb
@@ -33,7 +33,7 @@ class ScheduleWithCity
     age_limit = 45
     district_id = 371
 
-    response = URI.open("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={district_id}&date={date}",
+    response = URI.open("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=#{district_id}&date=#{date}",
     "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.51").read
 
     sessions = JSON.parse(response)
@@ -42,6 +42,7 @@ class ScheduleWithCity
     puts sessions.to_s
 
     if available_sessions.any?
+      system("available sessions")
       available_sessions.each do |session|
         if session['min_age_limit'] == age_limit
           system("say slot available at #{pin}")
